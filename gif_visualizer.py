50673e92ab8861c107da98fd98ad6d79eec6bfb3
Scott Bailey <scottrbailey@gmail.com>
2021-09-12 19:32:00 -0700

    Refactor to use Node class

--- gif_visualizer.py
+++ gif_visualizer.py
@@ -14,6 +14,7 @@ LED_COLS = 20
 LED_ROWS = 3
 LED_SIZE = 7
 SPACE_SIZE = 2
+LED_COUNT = LED_COLS * LED_ROWS
 FRAME_COUNT = LED_COLS * LED_ROWS
 NODE_IP = '192.168.10.208'
 OLD_URL = 'https://raw.githubusercontent.com/photocromax/WLED-live-visualizations/master/GIF/'
@@ -24,22 +25,50 @@ PALLET_EFFECT = 65
 
 image_size = (LED_COLS * (LED_SIZE + SPACE_SIZE),
               (LED_ROWS * (LED_SIZE + SPACE_SIZE)))
-# populated from [NODE_IP]/json
-effect_cnt = 0
-palette_cnt = 0
-effects = []
-palettes = []
+
 color_1 = 'FF6E41'
 color_2 = 'FFE369'
 color_3 = 'FFB9B8'
 
 
+class Node:
+    """ WLED node used to """
+    def __init__(self, ip):
+        self.ip = ip
+        req = requests.get(f'http://{ip}/json')
+
+        self.json = req.json()
+        self.palettes = self.json['palettes']
+        self.effects = self.json['effects']
+        self.palette_cnt = len(self.palettes)
+        self.effect_cnt = len(self.effects)
+        #
+        self.initialize()
+
+    def initialize(self):
+        # Set segment bounds and colors, set fx to solid pattern tri
+        api_cmd = {"on": True, "bri": 127,
+                   "seg": [{"start": 0, "stop": LED_COUNT - 1,
+                            "pal": 5, "fx": 84, "sx": 127, "ix": 0,
+                            "cols": [list(split_rgb(color_1)),
+                                     list(split_rgb(color_2)),
+                                     list(split_rgb(color_3))]}]}
+        req = requests.put(f'http://{self.ip}/json', json=api_cmd)
+
+    def liveview(self):
+        req = requests.get(f'http://{self.ip}/json/live')
+        return req.json()['leds']
+
+    def win(self, fx, fp, sx=127):
+        # HTTP Request API call
+        req = requests.get(f'http://{self.ip}/win&FP={fp}&FX={fx}&SX={sx}&TT=0')
+
+
 def draw_frame():
-    req = requests.get(f'http://{NODE_IP}/json/live')
     image = Image.new("P", image_size, "black")
     draw = ImageDraw.Draw(image)
-    leds = req.json()['leds']
-    for i in range(LED_COLS * LED_ROWS):
+    leds = node.liveview()
+    for i in range(LED_COUNT):
         row = i // LED_COLS
         col = LED_COLS - 1 - (i % LED_COLS) if row % 2 else i % LED_COLS
         x1 = col * (LED_SIZE + SPACE_SIZE) + 1
@@ -50,8 +79,8 @@ def draw_frame():
 
 def render_palette(fp=0):
     # Set effect to Palette and palette fp
-    req = requests.get(f'http://{NODE_IP}/win&FX={PALLET_EFFECT}&FP={fp}&TT=0&CL=h{color_1}&C2=h{color_2}&C3={color_3}')
-    print(f'rendering palette {fp} {palettes[fp]}')
+    node.win(PALLET_EFFECT, fp)
+    print(f'rendering palette {fp} {node.palettes[fp]}')
     time.sleep(0.3)
     frames = []
     for number in range(80):
@@ -62,7 +91,7 @@ def render_palette(fp=0):
                    save_all=True, duration=30, loop=0, quality=10)
 
 
-def render_effect(fx=0, fp=None):
+def render_effect(fx=0, fp=None, frame_cnt=None):
     # Set effect to fx and palette to Party
     if fp is None:
         # use fire palette for fire 2012 effect
@@ -71,12 +100,12 @@ def render_effect(fx=0, fp=None):
     slow_effects = [4, 5, 23, 24, 25, 32, 36, 58, 82, 90]
     if fx in slow_effects:
         sx = 255
-        frame_cnt = 120
+        frame_cnt = frame_cnt if frame_cnt is not None else 160
     else:
         sx = 127
-        frame_cnt = 80
-    req = requests.get(f'http://{NODE_IP}/win&FX={fx}&FP={fp}&SX={sx}&TT=0&CL=h{color_1}&C2=h{color_2}&C3={color_3}')
-    print(f'rendering effect {fx} {effects[fx]}')
+        frame_cnt = frame_cnt if frame_cnt is not None else 80
+    node.win(fx, fp, sx)
+    print(f'rendering effect {fx} {node.effects[fx]}')
     time.sleep(0.3)
     frames = []
     for number in range(frame_cnt):
@@ -88,28 +117,31 @@ def render_effect(fx=0, fp=None):
 
 
 def render_all_effects():
-    for i in range(0, len(node_info['effects'])):
+    for i in range(0, node.effect_cnt):
         render_effect(i)
 
 
 def render_all_palettes():
-    for i in range(0, len(node_info['palettes'])):
+    for i in range(0, node.palette_cnt):
         render_palette(i)
 
 
 def make_md():
     image = Image.new("P", (16, 16), "black")
     draw = ImageDraw.Draw(image)
-    draw.rectangle((1, 1, 14, 14), fill=split_rgb(int(color_1, 16)))
+    # reinitialize node so we can pull gamma corrected colors from it
+    node.initialize()
+    leds = node.liveview()
+    draw.rectangle((1, 1, 14, 14), fill=split_rgb(int(leds[0], 16)))
     image.save('gifs/color_1.gif', format="GIF")
-    draw.rectangle((1, 1, 14, 14), fill=split_rgb(int(color_2, 16)))
+    draw.rectangle((1, 1, 14, 14), fill=split_rgb(int(leds[1], 16)))
     image.save('gifs/color_2.gif', format="GIF")
-    draw.rectangle((1, 1, 14, 14), fill=split_rgb(int(color_3, 16)))
+    draw.rectangle((1, 1, 14, 14), fill=split_rgb(int(leds[2], 16)))
     image.save('gifs/color_3.gif', format="GIF")
 
     with open('effects.md', 'w') as fp:
         fp.write(f'''### Effects
-To aid in showing where colors vs palettes are used, all effects are rendered with the _{palettes[EFFECT_PALETTE]}_ palette  
+To aid in showing where colors vs palettes are used, all effects are rendered with the _{node.palettes[EFFECT_PALETTE]}_ palette  
 and the colors
 ![](gifs/color_1.gif) primary
 ![](gifs/color_2.gif) secondary
@@ -118,25 +150,20 @@ and the colors
 | ID | Effect | New Visual | Old Visual 
 | ---: | --- | --- | --- 
 ''')
-        for i in range(len(effects)):
-            fp.write(f'| {i} | {effects[i]} | ![](gifs/FX_{i:03d}.gif) | ![]({OLD_URL}/FX_{i}.gif)\n')
+        for i in range(node.effect_cnt):
+            fp.write(f'| {i} | {node.effects[i]} | ![](gifs/FX_{i:03d}.gif) | ![]({OLD_URL}/FX_{i}.gif)\n')
     with open('palettes.md', 'w') as fp:
-        fp.write(f'### Palettes\nPalettes are rendered with the {effects[PALLET_EFFECT]} effect\n')
+        fp.write(f'### Palettes\nPalettes are rendered with the {node.effects[PALLET_EFFECT]} effect\n')
         fp.write('| ID | Palette | New Vis | Old Vis \n| ---: | --- | --- | ---\n')
-        for i in range(len(palettes)):
-            fp.write(f'| {i} | {palettes[i]} | ![](gifs/PAL_{i:02d}.gif) | ![]({OLD_URL}/PAL_{i}.gif)\n')
+        for i in range(node.palette_cnt):
+            fp.write(f'| {i} | {node.palettes[i]} | ![](gifs/PAL_{i:02d}.gif) | ![]({OLD_URL}/PAL_{i}.gif)\n')
 
 
 if __name__ == "__main__":
     try:
-        req = requests.get(f'http://{NODE_IP}/json')
-        node_info = req.json()
+        node = Node(NODE_IP)
     except:
         print(f'Could not connect to WLED node at http://{NODE_IP}. Please configure NODE_IP.')
-    effects = node_info['effects']
-    palettes = node_info['palettes']
-    effect_cnt = node_info['info']['fxcount']
-    palette_cnt = node_info['info']['palcount']
 
     parser = argparse.ArgumentParser(description='Render effects and/or palettes as animated GIFs')
     parser.add_argument('-md', action='store_true', help='generate effect/palette markdown files')
